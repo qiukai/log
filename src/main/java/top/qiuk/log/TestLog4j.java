50fe0e1c8ee05303a8fff8255ec1ebf4fef3dad3
qiukai <871275049@qq.com>
2016-08-21 18:58:21 +0800

    添加error方法

--- src/main/java/top/qiuk/log/TestLog4j.java
+++ src/main/java/top/qiuk/log/TestLog4j.java
@@ -6,12 +6,67 @@ import java.io.StringWriter;
 
 import org.apache.log4j.Logger;
 
+import top.qiuk.log.db.DBUtil;
+
 public class TestLog4j {
 
 	private Logger logger = Logger.getLogger(getClass());
 
+	public static long time = 1L;
+
 	public static void main(String[] args) throws IOException {
-		new TestLog4j().test();
+		new TestLog4j().insertError();
+	}
+	
+	public void insertError() {
+		RuntimeException runtimeException = new RuntimeException();
+		runtimeException.printStackTrace();
+		
+		DBUtil.error("123", runtimeException);
+	}
+
+	public void insertDebug() {
+
+		for (int i = 0; i < 1000; i++) {
+			new Thread(new Runnable() {
+				public void run() {
+					try {
+						DBUtil.debug("342323");
+					} catch (Exception e) {
+						try {
+							Thread.sleep(time);
+							time *= 2;
+							DBUtil.error("123", e);
+							time = (long) time / 2;
+						} catch (Exception e1) {
+							try {
+								Thread.sleep(time);
+								time *= 2;
+								DBUtil.error("123", e);
+								time = (long) time / 2;
+							} catch (Exception e2) {
+								try {
+									Thread.sleep(time);
+									time *= 2;
+									DBUtil.error("123", e);
+									time = (long) time / 2;
+								} catch (Exception e3) {
+									try {
+										Thread.sleep(time);
+										time *= 2;
+										DBUtil.error("123", e);
+										time = (long) time / 2;
+									} catch (Exception e4) {
+										e4.printStackTrace();
+									}
+								}
+							}
+						}
+					}
+				}
+			}).start();
+		}
+
 	}
 
 	public void test() throws IOException {
@@ -19,7 +74,7 @@ public class TestLog4j {
 		RuntimeException runtimeException = new RuntimeException("123");
 
 		String stackTrace = null;
-		
+
 		StringWriter stringWriter = new StringWriter();
 		PrintWriter printWriter = new PrintWriter(stringWriter, true);
 		runtimeException.printStackTrace(printWriter);
--- src/main/java/top/qiuk/log/db/DBUtil.java
+++ src/main/java/top/qiuk/log/db/DBUtil.java
@@ -1,6 +1,7 @@
 package top.qiuk.log.db;
 
-import java.util.Date;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import org.bson.Document;
 
@@ -11,8 +12,8 @@ import top.qiuk.log.constant.LogLevelEnum;
 import top.qiuk.log.util.Id;
 
 public class DBUtil {
-	
-	public static DBUtil dbUtil= new DBUtil();
+
+	public static DBUtil dbUtil = new DBUtil();
 
 	public DBUtil() {
 		super();
@@ -44,27 +45,41 @@ public class DBUtil {
 		dbUtil.addLog(message, LogLevelEnum.ERROR);
 	}
 
+	public static void error(String message, Throwable throwable) {
+
+		StringWriter stringWriter = new StringWriter();
+		PrintWriter printWriter = new PrintWriter(stringWriter, true);
+		throwable.printStackTrace(printWriter);
+		message += (System.getProperty("line.separator") + stringWriter);
+		DBUtil.error(message);
+	}
+
 	public static void fatal(String message) {
 		dbUtil.addLog(message, LogLevelEnum.FATAL);
 	}
 
-	public void dbUtil(String message, LogLevelEnum logLevelEnum) {
-		int hashCode = message.hashCode();
-		int num = hashCode % DBList.getDBListSize();
+	public void addLog(String message, LogLevelEnum logLevelEnum) {
+		String id = Id.next();
+		int num = id.hashCode() % DBList.getDBListSize();
 		MongoDatabase mongoDatabase = DBList.getMongoDatabase(num);
 		MongoCollection<Document> collection = getCollection(mongoDatabase, logLevelEnum.getValue());
 		Document document = new Document();
-		document.append("_id", Id.next());
-		document.append("message", message);
-
+		document.append("_id", id);
 		StackTraceElement stack[] = Thread.currentThread().getStackTrace();
 		for (int i = 0; i < stack.length; i++) {
 			if (stack[i].getClassName().equals(getClass().getName())) {
-				System.out.println(stack[i+1].getClassName());
+				String className = stack[i + 1].getClassName();
+				String[] split = className.split("\\.");
+				String classSimpleName = split[split.length - 1];
+				String methodName = stack[i + 1].getMethodName();
+				int lineNumber = stack[i + 1].getLineNumber();
+				StringBuilder sb = new StringBuilder();
+				sb.append(className).append(".").append(methodName).append("(").append(classSimpleName)
+						.append(".java:").append(lineNumber).append(")");
+				document.append("class", sb.toString());
 			}
 		}
-
+		document.append("message", message);
 		collection.insertOne(document);
 	}
-
 }
